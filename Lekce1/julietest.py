a10f5d6c6429f602c2d4511238076fb73013da10
julie.myslivcova <julie.myslivcova@gmail.com>
2024-10-17 18:04:27 +0200

    Můj první commit

---
+++ Lekce1/julietest.py
@@ -0,0 +1,37 @@
+# def kontrola_emailu(data):
+#     chyby = 0
+#     for radek in data:
+#         if "@" not in radek:
+#             chyby = chyby + 1
+#     return chyby
+# emaily = ["misa@czechitas.cz",
+#           "kuba@czechitas.cz",
+#           "soustruh@czechitas.cz",
+#           "jirkaczechitas.cz"]
+
+# print(kontrola_emailu(emaily))
+
+
+pozice = {"Lenka": "testerka", "Lucka": "analytička"}
+ïf ..............:
+    print(pozice["Petra"])
+else:
+    print("Petru v seznamu nemáme")
+
+# nova_kolegyne = "Petra"
+# pozice[nova_kolegyne] = "vývojářka"
+# print(pozice)
+
+# oblibene_jazyky = {"Míša", "Python", "Lenka", "Java", "Petra", "SQL"}
+# print(len(oblibene_jazyky))
+
+# names = set()
+
+# names.add('Martin')
+# names.add('Jana')
+# names.add('Petr')
+# names.add('Simona')
+# print(len(names), names)
+
+# names.add('Martin')
+# print(len(names), names)
